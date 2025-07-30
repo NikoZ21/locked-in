e6ce9779a81d6732652aa130d6c20f751851ff78
ambrojobs <89398005+NikoZ21@users.noreply.github.com>
2025-07-30 10:37:40 +0400

    added app-layout image

--- src/components/Step.tsx
+++ src/components/Step.tsx
@@ -10,14 +10,18 @@ export default function Step({
   isLast = false,
 }: StepProps) {
   return (
-    <div className="relative flex flex-col items-center">
+    <div className="relative flex flex-col ">
       {/* Div 1 */}
-      <div
-        className={`flex justify-center items-center w-20 h-20 ${bgColor} rounded-full border-2 border-white z-10 relative`}
-      >
-        {number}
+      <div className="flex flex-row items-center gap-4">
+        <div
+          className={`flex justify-center items-center w-20 h-20 ${bgColor} rounded-full border-2 border-white z-10 relative`}
+        >
+          {number}
+        </div>
+        <div className="w-10 h-1 bg-gray-400 my-4"></div>
+        <h1 className="text-2xl font-bold">set up youtube tool project </h1>
       </div>
-      {!isLast && <div className="w-1 h-20 bg-gray-400 my-4"></div>}
+      {!isLast && <div className="w-1 h-20 bg-gray-400 my-4 ml-10"></div>}
     </div>
   );
 }
